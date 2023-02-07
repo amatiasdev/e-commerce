5a1aa73631fe5d9bcfa5d399573080bf9f55cefb
Aldo Matias <aldo_rheazyk@hotmail.com>
2023-02-06 18:21:32 -0600

    feature: Initial commit -Add Login page -Add Main page

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+import { Login } from './pages/Login/Login';
+import { Main } from './pages/Main/Main';
+
+
+
+function App() {
+
+  return (
+    <div className="App">
+      <Login></Login>
+      <Main></Main>
+    </div>
+  )
+}
+
+export default App
---
+++ src/pages/Main/Main.tsx
@@ -0,0 +1,3 @@
+import React from 'react';
+
+export  const Main = ({ message }: any): JSX.Element => <div>Main</div>;
